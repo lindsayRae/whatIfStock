6522053c35d00f583799efdc4a4bf75cffd7a1eb
lindsayRae <lbarnett712@gmail.com>
2020-11-01 15:26:37 -0500

    feat - finished authentication with session

--- server.js
+++ server.js
@@ -2,15 +2,34 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 1234
 const passport = require('./auth')
+
+const session = require('express-session')
 const routes = require('./routes')
 
 app.use(express.json());
 
 //app.use('/', routes)
+
+app.use(session({
+  secret: process.env.SESSION_SECRET || 'the secret',
+  resave: true,
+  saveUninitialized: false,
+  cookie: { secure: process.env.NODE_ENV === 'production' },
+
+}));
+
 app.use(passport.initialize())
 app.use(passport.session())
 
-app.get('/', (req, res) => {
+const isUserLoggedIn = (req, res, next) => {
+  if(req.user){
+    return next()
+  }
+
+  return res.status(403).send("You are not authorized")
+}
+
+app.get('/', isUserLoggedIn, (req, res) => {
   res.json([
       {id:1, title: 'Here is data from server.js'}
     ])
